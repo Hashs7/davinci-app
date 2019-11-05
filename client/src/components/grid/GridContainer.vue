07f33ea7d519f151e9843cec4ea6534b5c6c36fa
Sebastien Hernoux <seb_her@hotmail.fr>
2019-11-05 14:21:04 +0100

    Create grid

---
+++ client/src/components/grid/GridContainer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid-container">
+        <GridItem v-for="(item, i) in size*size" :key="i" :value="i" :style="cssVars"/>
+    </div>
+</template>
+
+<script>
+import GridItem from '@/components/grid/GridItem'
+export default {
+    name: "GridContainer",
+    components: {
+        GridItem
+    },
+    data() {
+        return {
+            size: 8
+        }
+    },
+    computed: {
+        cssVars() {
+            return {
+                '--grid-size': this.size,
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.grid-container {
+    display: inline-grid;
+    margin: 0 auto;
+    grid-template-columns: repeat(8, 40px);
+}
+</style>
---
+++ client/src/components/grid/GridItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid-item">
+        <span class="value">{{ value }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GridItem",
+    props: ['value'],
+}
+</script>
+
+<style scoped>
+.grid-item {
+    display: flex;
+    width: 40px;
+    height: 40px;
+    border: 2px solid black;
+}
+.value {
+    margin: auto;
+}
+</style>
