73aaa9a3caf4253c5636eadf2f31ff98b8c2656d
Sebastien Hernoux <seb_her@hotmail.fr>
2019-11-14 20:22:02 +0100

    Get sequence to move to start position

--- helpers.js
+++ helpers.js
@@ -5,8 +5,8 @@ exports.getSymbolCombination = (symbolId) => {
     return symbol.combination;
 };
 
-exports.getNewPosition = (allPositions, move) => {
-    const lastPos = allPositions.last();
+exports.getNewPositions = (allPositions, move) => {
+    const lastPos  = allPositions[allPositions.length - 1];
     const dronePos = allPositions;
     switch (move) {
         case "front":
@@ -24,4 +24,26 @@ exports.getNewPosition = (allPositions, move) => {
     }
     dronePos.push(lastPos);
     return dronePos
+};
+
+exports.convertPathToMoves = (path) => {
+    const moves = [];
+    path.forEach((el, i) => {
+        if(i === path.length - 1) return;
+        const nextEl = path[i+1];
+        if (el[0] > nextEl[0]) {
+            console.log("go left");
+            moves.push(constants.MOVE.left)
+        } else if (el[0] < nextEl[0]){
+            console.log("go right");
+            moves.push(constants.MOVE.right)
+        } else if (el[1] > nextEl[1]) {
+            console.log("go front");
+            moves.push(constants.MOVE.front)
+        } else if (el[1] < nextEl[1]) {
+            console.log("go back");
+            moves.push(constants.MOVE.back)
+        }
+    });
+    return moves;
 };
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const app = require('./app');
+const fs      = require('fs');
+const pf      = require('pathfinding');
+const app     = require('./app');
 const helpers = require('./helpers');
 
 const server = app.listen(process.env.PORT || 3000);
-const io = require('./socket').init(server);
+const io     = require('./socket').init(server);
 
 io.on('connection', socket => {
     console.log('Client connected');
+
 
     /*
     ** Swift app
@@ -19,17 +22,42 @@ io.on('connection', socket => {
 
     socket.on('droneMove', (moveStr) => {
         fs.readFile('matrix.json', 'utf8', (err, data) => {
-            if (err){
+            if (err) {
                 console.error(err);
                 return
             }
-            const { dronePositions } = JSON.parse(data);
-            const json = {
+            const {dronePositions} = JSON.parse(data);
+            const json             = {
                 ...data,
-                dronePositions: helpers.getNewPosition(dronePositions, moveStr)
+                dronePositions: helpers.getNewPositions(dronePositions, moveStr)
             };
-            fs.writeFile('matrix.json', JSON.stringify(json), 'utf8', () => {});
+            fs.writeFile('matrix.json', JSON.stringify(json), 'utf8', () => {
+            });
         });
+    });
+
+    socket.on('goToStart', () => {
+        fs.readFile('matrix.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return
+            }
+            const {dronePositions, matrix, start} = JSON.parse(data);
+            const currentPos                      = dronePositions[dronePositions.length - 1];
+            const grid                            = new pf.Grid(matrix.nodes);
+
+            grid.nodes.forEach((el, i) => {
+                el.forEach((subEl, j) => {
+                    grid.setWalkableAt(j, i, true)
+                })
+            });
+            const finder = new pf.AStarFinder();
+            const path   = finder.findPath(currentPos.x, currentPos.y, start.x, start.y, grid);
+            const moves  = helpers.convertPathToMoves(path);
+            console.log(moves);
+            io.emit('goToStart', moves)
+        });
+
     });
 
     /*
